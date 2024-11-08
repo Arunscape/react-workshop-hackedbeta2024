15a930995aa50d684a50640830ba1b1f88d6e2db
Arunscape <8227297+Arunscape@users.noreply.github.com>
2024-11-08 16:18:48 -0700

    add repo link at end

--- app/questions/page.tsx
+++ app/questions/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function () {
     return (
         <div className="content-center">
@@ -5,6 +8,15 @@ export default function () {
             <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
                 Questions?
             </h1>
+
+            <div className="h-10"></div>
+            <Link href="https://github.com/new?template_name=react-workshop-hackedbeta2024&template_owner=Arunscape">
+                <Button>Want to use this website as a template?</Button>
+            </Link>
+            <div className="h-10"></div>
+            <div className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-4xl">
+                github.com/Arunscape/react-workshop-hackedbeta2024
+            </div>
         </div>
     )
 }
